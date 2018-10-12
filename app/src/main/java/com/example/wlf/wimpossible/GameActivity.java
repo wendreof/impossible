b11788cdfa5ecff6386844c480838005acfde0a2
wlf <wendreoofernandes@gmail.com>
2018-10-12 00:57:03 -0300

    Score bug fixes and Add Sprites

--- app/src/main/java/com/example/wlf/wimpossible/GameActivity.java
+++ app/src/main/java/com/example/wlf/wimpossible/GameActivity.java
@@ -2,10 +2,12 @@ package com.example.wlf.wimpossible;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnTouchListener {
 
     Impossible view;
 
@@ -22,16 +24,41 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         //Logical
-        view = new Impossible(this);
+        view = new Impossible( this );
+
+        view.setOnTouchListener( this );
 
         //View Setup
-        setContentView(view);
+        setContentView( view );
 
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         view.resume();
+    }
+
+    @Override
+    public boolean onTouch( View v, MotionEvent motionEvent )
+    {
+        if ( motionEvent.getX() < 100 && motionEvent.getY() > 290 && motionEvent.getY() < 310 )
+        {
+            view.init();
+        }
+
+        //Exit
+        if ( motionEvent.getX() < 100 && motionEvent.getY() > 490 && motionEvent.getY() < 510 )
+        {
+            System.exit( 0 );
+        }
+
+        // Incrementa em 10 pixel a posição
+        // Vertical do player e o placar
+        view.moveDown( 10 );
+        view.addScore( 100 );
+
+        return true;
     }
 }
--- app/src/main/java/com/example/wlf/wimpossible/Impossible.java
+++ app/src/main/java/com/example/wlf/wimpossible/Impossible.java
@@ -1,6 +1,7 @@
 package com.example.wlf.wimpossible;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -11,8 +12,15 @@ public class Impossible extends SurfaceView implements Runnable {
 
     boolean running = false;
     Thread renderThread = null;
+
     SurfaceHolder holder;
     Paint paint;
+
+    private int playerY = 300,  playerX = 300, playerRadius = 50;
+    private float enemyX, enemyY, enemyRadius = 50;
+    private double distance;
+    private boolean gameover;
+    private int score;
 
     public Impossible( Context context )
     {
@@ -23,15 +31,39 @@ public class Impossible extends SurfaceView implements Runnable {
 
     private void drawPlayer(Canvas canvas)
     {
-        paint.setColor(Color.GREEN);
-        canvas.drawCircle(100, 100, 100, paint);
+        paint.setColor( Color.GREEN );
+        //canvas.drawCircle( playerX, playerY, 50, paint );
+        canvas.drawBitmap( BitmapFactory.decodeResource( getResources(),
+                R.drawable.nave ), playerX-50, playerY-50, null );
+    }
+
+    private void drawEnemy( Canvas canvas )
+    {
+        paint.setColor( Color.RED );
+        enemyRadius++;
+
+        canvas.drawCircle( enemyX, enemyY, enemyRadius, paint );
     }
 
     public void resume()
     {
         running = true;
-        renderThread = new Thread(this);
+        renderThread = new Thread( this );
         renderThread.start();
+    }
+
+    private void checkCollision( Canvas canvas )
+    {
+        //calcula a hipotenusa
+
+        distance = Math.pow( playerY - enemyY, 2 ) + Math.pow( playerX - enemyX, 2 );
+        distance = Math.sqrt( distance );
+
+        //verifica distância entre os raios
+        if ( distance <= playerRadius + enemyRadius )
+        {
+            gameover = true;
+        }
     }
 
     @Override
@@ -46,14 +78,82 @@ public class Impossible extends SurfaceView implements Runnable {
 
             //bloqueia o canvas
             Canvas canvas = holder.lockCanvas();
+            //canvas.drawColor( Color.BLACK );
+            canvas.drawBitmap( BitmapFactory.decodeResource( getResources(), R.drawable.sky ),
+                    0, 9, null );
 
-            //desenha o player
+            //desenha o player e o inimigo
             drawPlayer( canvas );
+            drawEnemy( canvas );
+
+            //detecta a colisao
+            checkCollision( canvas );
+
+            //game over
+            if ( gameover )
+            {
+                stopGame( canvas );
+                break;
+            }
+
+            //atualiza o placar
+            drawScore( canvas );
+
+            //Restart e Exit
+            drawButtons( canvas );
 
             //atualiza e libera o canvas
             holder.unlockCanvasAndPost( canvas );
         }
     }
 
+    public void moveDown( int pixels )
+    {
+        playerY += pixels;
+    }
+
+    private void stopGame( Canvas canvas )
+    {
+        paint.setStyle( Paint.Style.FILL );
+        paint.setColor( Color.LTGRAY );
+        paint.setTextSize( 100 );
+        canvas.drawText(  "Game Over!",  50, 150, paint );
+    }
+
+    public void addScore( int points )
+    {
+        score += points;
+    }
+
+    public void drawScore( Canvas canvas )
+    {
+        paint.setStyle( Paint.Style.FILL );
+        paint.setColor( Color.WHITE );
+        paint.setTextSize( 50 );
+        canvas.drawText( String.valueOf( score ), 50 , 200, paint);
+    }
+
+    public void drawButtons ( Canvas canvas )
+    {
+        //Restart
+        paint.setStyle( Paint.Style.FILL );
+        paint.setColor( Color.WHITE );
+        paint.setTextSize( 30 );
+        canvas.drawText( "Restart", 50, 300, paint );
+
+        //Exit
+        paint.setStyle( Paint.Style.FILL );
+        paint.setColor( Color.WHITE );
+        paint.setTextSize( 30 );
+        canvas.drawText( "Exit", 50, 500, paint );
+    }
+
+    public void init()
+    {
+        enemyX = enemyY = enemyRadius =0;
+        playerX = playerY = 300;
+        playerRadius = 50;
+        gameover = false;
+    }
 
 }
